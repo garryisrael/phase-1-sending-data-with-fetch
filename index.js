a9b3e2c2597ce8f3e6d782feaac577f924415c9f
Garry <garryisrael02@gmail.com>
2023-06-24 06:05:57 -0400

    sends Data with POST

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const { bodyParser } = require("json-server");
+
 // Add your code here
+function submitData(name, email) {
+  const user ={
+    name,
+    email
+  }
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+  .then(response => response.json())
+  .then(userResponse =>{
+    const body=document.querySelector("body")
+    const p = document.createElement("p")
+    p.textContent = userResponse.id
+    body.appendChild(p)
+  })
+    .catch(error =>{
+      const body=document.querySelector("body")
+      const p = document.createElement("p")
+      p.textContent = error.message
+      body.appendChild(p)
+    })
+}
